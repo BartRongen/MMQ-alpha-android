04835a1d4002e1505f381c7d1bbd06547695ad44
Bart Rongen <bart@appsss.nl>
2016-01-28 01:05:44 +0100

    update

--- app/src/main/java/com/example/bartrongen/mmqalpha/PlayerActivity.java
+++ app/src/main/java/com/example/bartrongen/mmqalpha/PlayerActivity.java
@@ -4,14 +4,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -22,6 +32,7 @@ import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -49,37 +60,113 @@ import java.util.List;
  */
 public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener, YouTubePlayer.PlaybackEventListener {
 
-    private YouTubePlayerView playerView;
     Context context = this;
     List<String> video_code_array, video_title_array;
     YouTubePlayer player;
     boolean fix = true;
     Toolbar mToolbar;
-    ListView lv_upcoming;
-    LinearLayout ll_search;
     ArrayAdapter<String> arrayAdapter;
     String currentTitle;
+    Handler handler;
+    private List<VideoItem> searchResults;
+    ArrayAdapter<VideoItem> adapter;
 
+    //injecting views
     @InjectView(R.id.now_playing) TextView now_playing;
+    @InjectView(R.id.search_input) EditText searchInput;
+    @InjectView(R.id.videos_found) ListView videosFound;
+    @InjectView(R.id.player_view) YouTubePlayerView playerView;
+    @InjectView(R.id.list_upcoming) ListView lv_upcoming;
+    @InjectView(R.id.linlay_search) LinearLayout ll_search;
 
     @Override
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
-
+        //sets the contentview
         setContentView(R.layout.activity_player);
+        //configure toolbar
         mToolbar = (Toolbar) findViewById(R.id.mtoolbar);
         mToolbar.setTitle(getIntent().getStringExtra("channel"));
         mToolbar.inflateMenu(R.menu.menu_main);
         mToolbar.setTitleTextColor(-1);
-
+        //inject ButterKnife for onclick methods and views
         ButterKnife.inject(this);
+        //search handler
+        handler = new Handler();
 
-        playerView = (YouTubePlayerView)findViewById(R.id.player_view);
-
-        lv_upcoming = (ListView)findViewById(R.id.list_upcoming);
-        ll_search = (LinearLayout)findViewById(R.id.linlay_search);
-
+        searchInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    searchOnYoutube(v.getText().toString());
+                    return false;
+                }
+                if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                    searchOnYoutube(v.getText().toString());
+                    loseFocus();
+                    return false;
+                }
+                return true;
+            }
+        });
+        addClickListener();
+        //run initial api call
         new getUpcoming().execute();
+    }
+
+    private void loseFocus(){
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(this.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    private void searchOnYoutube(final String keywords){
+        new Thread(){
+            public void run(){
+                YoutubeConnector yc = new YoutubeConnector(PlayerActivity.this);
+                searchResults = yc.search(keywords);
+                handler.post(new Runnable(){
+                    public void run(){
+                        updateVideosFound();
+                    }
+                });
+            }
+        }.start();
+    }
+
+    private void updateVideosFound(){
+        adapter = new ArrayAdapter<VideoItem>(getApplicationContext(), R.layout.video_item, searchResults){
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if(convertView == null){
+                    convertView = getLayoutInflater().inflate(R.layout.video_item, parent, false);
+                }
+                ImageView thumbnail = (ImageView)convertView.findViewById(R.id.video_thumbnail);
+                TextView title = (TextView)convertView.findViewById(R.id.video_title);
+                TextView description = (TextView)convertView.findViewById(R.id.video_description);
+
+                VideoItem searchResult = searchResults.get(position);
+
+                Picasso.with(getApplicationContext()).load(searchResult.getThumbnailURL()).into(thumbnail);
+                title.setText(searchResult.getTitle());
+                description.setText(searchResult.getDescription());
+                return convertView;
+            }
+        };
+
+        videosFound.setAdapter(adapter);
+    }
+
+    private void addClickListener(){
+        videosFound.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> av, View v, int pos,
+                                    long id) {
+                Toast.makeText(context, "Added video to the list", Toast.LENGTH_SHORT).show();
+                video_code_array.add(searchResults.get(pos).getId());
+                video_title_array.add(searchResults.get(pos).getTitle());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
@@ -120,13 +207,6 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
         }
     }
 
-    @OnClick(R.id.add_to_broadcast)
-    public void addToBroadcast(View v) {
-        Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
-        startActivity(intent);
-    }
-
-
     @OnClick(R.id.refresh)
     public void refresh(View v){
         new getUpcoming().execute();
@@ -134,6 +214,13 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
 
     @OnClick(R.id.tv_upcoming)
     public void upcomingClicked(View v){
+        loseFocus();
+        //clear videosfound list
+        try {
+            searchResults.clear();
+            adapter.notifyDataSetChanged();
+        } catch (NullPointerException e){}
+
         ll_search.setVisibility(View.GONE);
         lv_upcoming.setVisibility(View.VISIBLE);
     }
@@ -142,6 +229,7 @@ public class PlayerActivity extends YouTubeBaseActivity implements YouTubePlayer
     public void addVideoClicked(View v){
         lv_upcoming.setVisibility(View.GONE);
         ll_search.setVisibility(View.VISIBLE);
+        searchInput.setText("");
     }
 
     public void removeFromBroadcastList(View v) {
