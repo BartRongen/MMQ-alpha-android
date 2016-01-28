2e5fbe81c3f570b592944b9363ea53a8bd2baeb1
Bart Rongen <bart@appsss.nl>
2016-01-28 15:36:06 +0100

    added alertdialog for adding channels

--- app/src/main/java/com/example/bartrongen/mmqalpha/MainActivity.java
+++ app/src/main/java/com/example/bartrongen/mmqalpha/MainActivity.java
@@ -1,18 +1,38 @@
 package com.example.bartrongen.mmqalpha;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+import butterknife.OnClick;
+import butterknife.Optional;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -26,26 +46,94 @@ import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
     Context context = this;
-    ListView lv;
     List<String> title_array, slug_array;
+    EditText dialogText;
+
+    @InjectView(R.id.listView) ListView lv;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
         this.setTitle("MMQalpha");
-        lv = (ListView) findViewById(R.id.listView);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Object slug = slug_array.get(position);
-                setTitle(slug.toString());
                 Intent intent = new Intent(getApplicationContext(), PlayerActivity.class);
                 intent.putExtra("channel", slug.toString());
                 startActivity(intent);
             }
         });
         new getData().execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.add_channel:
+                createAddChannelDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void createAddChannelDialog() {
+        final EditText input = new EditText(context);
+        input.setHint("channel name");
+        input.setSingleLine(true);
+        final AlertDialog dlg = new AlertDialog.Builder(this).
+                setTitle("Add a channel").
+                setView(input).
+                setCancelable(false).
+                setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(final DialogInterface dialog, final int which) {
+                        String value = input.getText().toString().trim();
+                        Toast.makeText(getApplicationContext(), value,
+                                Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                }).
+                setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(final DialogInterface dialog, final int which) {
+                        dialog.dismiss();
+                    }
+                }).create();
+        input.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    String value = input.getText().toString().trim();
+                    Toast.makeText(getApplicationContext(), value,
+                            Toast.LENGTH_SHORT).show();
+                    dlg.dismiss();
+                    return false;
+                }
+                return true;
+            }
+        });
+        dlg.setOnShowListener(new DialogInterface.OnShowListener()
+        {
+            @Override
+            public void onShow(final DialogInterface dialog)
+            {
+                input.requestFocus();
+                ((InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE)).showSoftInput(input, 0);
+            }
+        });
+        dlg.show();
     }
 
     class getData extends AsyncTask<String, String, String> {
